4c959b8fcd499b268e408e1795a80931f85e1677
dandankarai <cunhacb22@gmail.com>
2021-01-25 16:29:34 -0300

    upgrade

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
-import Sidebar from './Sidebar';
-import Content from './Content';
 import React, { Component } from 'react';
 
+import { Github } from '@styled-icons/boxicons-logos';
+import { LinkedinSquare } from '@styled-icons/boxicons-logos/'
+
+import { Info } from './styles'
+import { Content } from './styles'
+
 class App extends Component {
-  render(){
-    return(
-      <div>
+  render() {
+    return (
+      <>
 
-        <Sidebar />
-        <Content />
+        {/* <div alt="Info" > */}
+          <Info>
+            <div>
+              <img src="https://avatars.githubusercontent.com/u/65553049?s=460&u=7616301b1e4aad0c062411c373c9c42c7f2ec8ef&v=4" /> <br />
+              <span>Daniel Cunha</span> <br />
+              <a href="https://github.com/dandankarai"> <Github size="40" /></a>
+              <a href="https://www.linkedin.com/in/daniel-cunha001"> <LinkedinSquare size="43" /> </a>
+            </div>
+          </Info>
+        {/* </div> */}
 
-      </div>
+          <Content>
+            <div className="Conteudo" >
+              <span> Daniel Cunha Barbosa </span> <br />
+              <p>
+                Formado em técnico em Informática integrado ao Ensino Médio,
+                pelo Instituto Federal Goiano Campus Ceres, atuou na área
+                administrativa na empresa Cooper Rubi Açucar e Álcool na cidade
+                de Rubiataba, atuando na área por volta de 5 meses, desde então
+                dedica seu tempo a cursos para encontrar um emprego na área de Ti.
+              </p>
+
+            <h3>Conhecimentos</h3>
+              <ul>
+                <li>Html/Css</li>
+                <li>Javascript</li>
+                <li>React/React Native</li>
+                <li>NodeJs</li>
+              </ul>
+
+              <h3>Projetos</h3>
+              <ul>
+                <li>Calculadora (Seguindo o padrão da calculadora do IOs/Mac feita com React Native)</li>
+              </ul>
+            </div>
+            
+          </Content>
+      </>
     )
   }
 }
--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,43 @@
 import styled from 'styled-components';
 
-export const Icone = styled.Icone`
-  border-radius:50%;
+export const Info = styled.div`
+  /* background-color:red; */
+  margin-top:10px;
+  font-size:25px;
+  float:left;
+  font-family:'Times New Roman', Times, serif;
+  img{
+      border-radius:50%;
+      max-width:180px;
+      max-height:180px;
+      width: auto;
+      height:auto;
+    }
+  a{
+    color: black;
+    padding:5px;
+    text-decoration:none;
+  }
 `;
+
+export const Content = styled.div`
+  /* background-color:blue; */
+  display:flex;
+  align-items:stretch;
+  padding:15px;
+  font-size:20px;
+  /* font-family:'Times New Roman', Times, serif; */
+    span{
+      font-size:40px;
+    }
+    p{
+      font-size:20px;
+      text-align:justify;
+      
+    }
+    ul{
+      list-style:circle;
+    }
+`;
+
+
